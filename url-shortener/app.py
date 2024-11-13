ea2e3a6bb77467d4103d42689016e106ecb31b29
sporestudio <jrodcas@proton.me>
2024-11-13 23:52:04 +0100

    fix: correct the app.py and modify its functions
    Modify the index function to manage GET requests and implement the
    create txt register in the DNS server. Fix the redirect url function
    which now implements the get original url making a query to the TXT
    register in DNS server.

---
+++ url-shortener/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect
+from dns_manager import create_txt_record, get_original_url
+from dotenv import load_dotenv
+import hashlib
+import os
+
+
+load_dotenv(dotenv_path='.env')
+
+app = Flask(__name__)
+
+# Global variables
+DOMAIN_NAME = os.getenv('DOMAIN_NAME')
+
+
+def shortener_url(original_url):
+    return hashlib.sha256(original_url.encode()).hexdigest()[:6]
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        original_url = request.form['url']
+        short_url = shortener_url(original_url) 
+        
+        if create_txt_record(short_url, original_url):
+            url_shortener = f"https://{DOMAIN_NAME}/{short_url}"
+            return render_template('index.html', short_url=url_shortener, original_url=original_url)
+        else:
+            return "Error creating DNS TXT record", 500
+
+    return render_template('index.html')
+    
+
+@app.route('/<short_url>')
+def redirect_url(short_url):
+    original_url = get_original_url(short_url)
+    if original_url:
+        return redirect(original_url)
+    else:
+        return "Short url not found", 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
+
+
+
+
+
+
+
