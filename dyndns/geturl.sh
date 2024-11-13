ec01a5f058fb4ef317c0b5d2fdcdb1da054debce
sporestudio <jrodcas@proton.me>
2024-11-14 00:03:26 +0100

    feat: add geturl script
    Add geturl.sh file so that the user can make a request to the DYNDNS in
    order to obtain the url that update the dynamic IP..

---
+++ dyndns/geturl.sh
@@ -0,0 +1,20 @@
+API_KEY=$(cat user.key)
+output=$(curl -X "POST" "https://api.hosting.ionos.com/dns/v1/dyndns" \
+    -H "accept: application/json" \
+    -H "X-API-Key: ${API_KEY}" \
+    -H "Content-Type: application/json" \
+    -d '{
+        "domains": [
+            "sporestudio.me",
+            "www.sporestudio.me",
+            "url.sporestudio.me"
+        ],
+        "description": "Dynamic DNS"
+    }') 
+
+# process the curl and getting the updateUrl using jq
+updateUrl=$(echo "$output" | jq -r '.updateUrl')
+
+# save the url into a file and output its content
+echo $updateUrl > update_url
+cat update_url
