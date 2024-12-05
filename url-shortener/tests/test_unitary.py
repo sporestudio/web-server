c49c9b07c3c998480604502dfd32065b55c08c43
sporestudio <jrodcas@proton.me>
2024-12-05 14:15:46 +0100

    feat: add unitary test for the app
    Add a test to check that the url shortener function is working properly.

---
+++ url-shortener/tests/test_unitary.py
@@ -0,0 +1,8 @@
+import pytest
+from app import shortener_url
+
+def test_generate_short_url():
+    original_url = "https://"
+    short_url = shortener_url(original_url)
+    assert len(short_url) == 6
+    assert short_url.isalnum()
