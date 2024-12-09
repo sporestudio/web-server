20edbbb09717a5c4ab06a999b364e0b3f65b3719
sporestudio <jrodcas@proton.me>
2024-12-10 00:12:03 +0100

    fix: correct example original url in unitary test

--- url-shortener/tests/test_unitary.py
+++ url-shortener/tests/test_unitary.py
@@ -2,7 +2,7 @@ import pytest
 from app import shortener_url
 
 def test_generate_short_url():
-    original_url = "https://"
+    original_url = "https://example.com"
     short_url = shortener_url(original_url)
     assert len(short_url) == 6
     assert short_url.isalnum()
